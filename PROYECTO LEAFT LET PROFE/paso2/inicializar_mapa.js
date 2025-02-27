0f91de1cc34f85f15b54d97e7fdba21964616f6d
Mike RR <michaelandresrr@gmail.com>
2025-02-26 19:31:54 -0500

    h

---
+++ PROYECTO LEAFT LET PROFE/paso2/inicializar_mapa.js
@@ -0,0 +1,29 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+map.pm.addControls({
+    position: 'topleft',
+    drawCircle: false,
+    drawCircleMarker: false,
+    drawRectangle: false,
+    drawPolygon: true,
+    drawPolyline: true,
+    drawMarker: true,
+    cutPolygon: false,
+    removalMode: false,
+})
+let drawlayers =[];
+let intersectionsLayers = [];
+
+// Manejadores de eventos
+map.on("pm:create", function(e){
+    let mylayer = e.layer;
+    //si es un poligono entonces agregarlo al arreglo drawlayer
+    if(mylayer instanceof L.Polygon){
+        drawlayers.push(mylayer);
+    }
+
+})
