2ace3c3dc7863af3f88d0e85bd31445cfd439b7b
Mike RR <michaelandresrr@gmail.com>
2025-02-26 21:08:43 -0500

    Refactor area calculation and improve area label styling

--- PROYECTO LEAFT LET PROFE/paso2/analistas.js
+++ PROYECTO LEAFT LET PROFE/paso2/analistas.js
@@ -15,16 +15,13 @@ function calculateareas() {
         try {
             let area = turf.area(layer.toGeoJSON());
             let centroid = turf.centroid(layer.toGeoJSON());
-            x = centroid.geometry.coordinates[1];
-            y = centroid.geometry.coordinates[0];
-            L.marker([x, y],{
-                icon:L.divIcon(
-                    {
-                        className: "area_label",
-                        html: `${area} m<sup>2</sup>`
-                    }
-                ),
-
+            let x = centroid.geometry.coordinates[1];
+            let y = centroid.geometry.coordinates[0];
+            L.marker([x, y], {
+                icon: L.divIcon({
+                    className: "area_label",
+                    html: `${area.toFixed(2)} m<sup>2</sup>`
+                }),
             }).addTo(map);
             console.log("Área del polígono:", area, "metros cuadrados");
         } catch (error) {
